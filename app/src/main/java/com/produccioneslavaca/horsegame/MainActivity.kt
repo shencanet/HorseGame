0216ea65d4fb2fb93c932825c9869084b9b0ce49
shen <santi.dionis@gmail.com>
2023-12-28 21:21:41 +0100

    ADD: FUNCTION LOGIC GAME

--- app/src/main/java/com/produccioneslavaca/horsegame/MainActivity.kt
+++ app/src/main/java/com/produccioneslavaca/horsegame/MainActivity.kt
@@ -2,10 +2,40 @@ package com.produccioneslavaca.horsegame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initScreenGame()
+
+
+    }
+
+    private fun initScreenGame(){
+        setSizeBoard()
+        hide_message()
+
+    }
+
+
+    private fun setSizeBoard(){
+        var iv: ImageView
+        for (i in 0..7){
+            for(j in 0..7){
+                iv = findViewById(resources.getIdentifier("c$i$j","id" , packageName))
+
+            }
+        }
+
+    }
+
+    private fun hide_message(){
+        var lyMessage = findViewById<LinearLayout>(R.id.lyMessage)
+        lyMessage.visibility = View.INVISIBLE
     }
 }
