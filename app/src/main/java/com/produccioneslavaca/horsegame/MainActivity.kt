e6d2bd52bc520624242a32495050307fd6f5c560
Santi Dionis <96749556+shencanet@users.noreply.github.com>
2024-01-06 21:45:59 +0100

    Update MainActivity.kt

--- app/src/main/java/com/produccioneslavaca/horsegame/MainActivity.kt
+++ app/src/main/java/com/produccioneslavaca/horsegame/MainActivity.kt
@@ -112,7 +112,7 @@ class MainActivity : AppCompatActivity() {
         var option_y = y + mov_y
 
         if(option_x <8 && option_y < 8 && option_x >= 8 && option_y >= 8){
-            if (board[option_x == 0][option_y == 0] || board[option_x == 0][option_y == 2]
+            if (board[option_x][option_y] == 0 || board[option_x][option_y]==2
                ){
                 option++
                 paintOptions(option_x, option_y)
