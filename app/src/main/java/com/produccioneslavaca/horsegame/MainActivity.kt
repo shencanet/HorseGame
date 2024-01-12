64372b1a2de825eba6ca238fc016644bcd1c0473
Santi Dionis <96749556+shencanet@users.noreply.github.com>
2024-01-12 21:00:35 +0100

    Update MainActivity.kt

--- app/src/main/java/com/produccioneslavaca/horsegame/MainActivity.kt
+++ app/src/main/java/com/produccioneslavaca/horsegame/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private var cellSelected_x = 0
     private var cellSelected_y = 0
     private var options = 0
+    private var width_bonus = 0
     private var movesRequired = 4
     private var moveoptions = 64
     private var bonus = 0
