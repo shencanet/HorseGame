d6165b5000e9508364448103ee3e5cf9feb96176
Santi Dionis <96749556+shencanet@users.noreply.github.com>
2024-01-22 20:05:26 +0100

    Update apuntesKotlin.kt

--- apuntesKotlin.kt
+++ apuntesKotlin.kt
@@ -1,1 +1,18 @@
 
+fun main(args: Array<String>) {
+    val lista = listOf("10", "11", "12")//lista
+    val Conjunto = setOf(1, 7 , 11)// elemento sin valores repetidos
+    val diccionario = mapOf(1 to "one", 7 to "seven")//pares compuestos una clave y un valor
+
+    fun isLetter(c: Char): Boolean = c in 'a'..'z' || c in 'A'..'Z'
+
+    for ((index, element) in lista.withIndex()) {
+        println("$element")
+        println("$index")
+        println(isLetter('a'))  // Pass a Char, not a String
+        
+        //usar lista
+       val resultadSuma =  lista.sum();
+        
+    }
+}
